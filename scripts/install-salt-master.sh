67d40242b6915ec17837beafe0ce2024d915528b
Benjamin Schneider <ich@benjamin-schneider.com>
2022-05-29 11:14:55 +0200

    fix salt installer

--- scripts/install-salt-master.sh
+++ scripts/install-salt-master.sh
@@ -10,8 +10,8 @@ wget -O /tmp/install-salt.sh https://bootstrap.saltstack.com
 chmod +x /tmp/install-salt.sh
 
 # Install salt
-/tmp/install-salt.sh    -M \        # Also install salt-master
-                        -L \        # Also install salt-cloud and required python-libcloud package
-                        -A main     # Pass the salt-master DNS name or IP. This will be stored under ${BS_SALT_ETC_DIR}/minion.d/99-master-address.conf
+/tmp/install-salt.sh -M -L -A main  # Also install salt-master
+                                    # Also install salt-cloud and required python-libcloud package
+                                    # Pass the salt-master DNS name or IP. This will be stored under ${BS_SALT_ETC_DIR}/minion.d/99-master-address.conf
 
 
