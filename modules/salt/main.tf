8329ba11015d635d33932d20b2cafb9580c4e338
Benjamin Schneider <ich@benjamin-schneider.com>
2022-06-02 11:57:58 +0200

    i love oauth

--- modules/salt/main.tf
+++ modules/salt/main.tf
@@ -123,7 +123,7 @@ data "github_repository" "goinput-terraform" {
 }
 
 resource "github_repository_webhook" "goinput-terraform_salt_hook" {
-  repository = "goINPUT-IT-Solutions/terraform-goinput"
+  repository = data.github_repository.goinput-terraform.name
 
   configuration {
     url          = "https://${hcloud_server.saltbastion.name}/hook/github"
