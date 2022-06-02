8034d8a317ebd58cc508b53d1e7c468965c9fed7
Benjamin Schneider <ich@benjamin-schneider.com>
2022-06-02 10:55:24 +0200

    Outdated doku wuhuuu

--- modules/salt/main.tf
+++ modules/salt/main.tf
@@ -123,7 +123,7 @@ data "github_repository" "goinput-terraform" {
 }
 
 resource "github_repository_webhook" "goinput-terraform_salt_hook" {
-  repository = data.github_repository.goinput-terraform.name
+  repository = "goINPUT-IT-Solutions/terraform-goinput"
 
   name = "Salt Hook"
 
